ea6566cf2cb19908c96cc49b6055c2e4d39c832a
yunsful <bailey05280@naver.com>
2024-11-17 22:26:52 +0900

    feat: 회원가입 관련 DTO 작성

--- 유니-김승윤/spring/src/main/java/umc/spring/web/dto/MemberDto.java
+++ 유니-김승윤/spring/src/main/java/umc/spring/web/dto/MemberDto.java
@@ -1,6 +1,37 @@
 package umc.spring.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class MemberDto {
+    
+    @Getter
+    public static class JoinDTO {
+        
+        private String password;
+        private String name;
+        private Integer gender;
+        private LocalDate birthday;
+        private String address;
+        private String phone;
+        private String email;
+        private List<Long> preferCategory;
+    }
+    
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class JoinResultDTO{
+        Long memberId;
+        LocalDateTime createdAt;
+    }
     
     public static class MemberProfileDto {
         private String nickname;
