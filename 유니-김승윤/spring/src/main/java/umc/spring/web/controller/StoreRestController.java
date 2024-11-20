45c489055ae5ec1beb89ea25a83d748babf19566
yunsful <bailey05280@naver.com>
2024-11-21 01:18:38 +0900

    feat1: 컨트롤러 추가

--- 유니-김승윤/spring/src/main/java/umc/spring/web/controller/StoreRestController.java
+++ 유니-김승윤/spring/src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -1,2 +1,24 @@
-package umc.spring.web.controller;public class StoreRestController {
+package umc.spring.web.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.service.storeService.StoreService;
+import umc.spring.web.dto.StoreDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stores")
+public class StoreRestController {
+    private final StoreService storeService;
+    
+    @PostMapping("/regions")
+    public ApiResponse<String> addRegionToStore(@RequestBody @Valid StoreDTO.StoreToRegion req) {
+        storeService.addStoreToRegion(req);
+        return ApiResponse.onSuccess("성공하였습니다.");
+    }
 }
