41660489ed8e046dae6df80a01797f400374f27a
yunsful <bailey05280@naver.com>
2024-11-20 17:33:20 +0900

    수정

--- 유니-김승윤/spring/src/main/java/umc/spring/Application.java
+++ 유니-김승윤/spring/src/main/java/umc/spring/Application.java
@@ -1,12 +1,8 @@
 package umc.spring;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import umc.spring.service.StoreQueryService;
 
 @SpringBootApplication
 @EnableJpaAuditing
