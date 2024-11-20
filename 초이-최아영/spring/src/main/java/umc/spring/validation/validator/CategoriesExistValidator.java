c7993f218d6e10482ea9f3b4f523a8f793ccb9f7
choiao0 <qawse6@daum.net>
2024-11-21 01:41:06 +0900

    Feat: 어노테이션을 활용한 validation 구현

---
+++ 초이-최아영/spring/src/main/java/umc/spring/validation/validator/CategoriesExistValidator.java
@@ -0,0 +1,36 @@
+package umc.spring.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.repository.CategoryRepository.CategoryRepository;
+import umc.spring.validation.annotation.ExistCategories;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public void initialize(ExistCategories constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
+        boolean isValid = values.stream()
+                .allMatch(value -> categoryRepository.existsById(value));
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.CATEGORY_NOT_FOUND.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+    }
+}
--- 초이-최아영/spring/src/main/java/umc/spring/web/dto/MemberRequestDTO.java
+++ 초이-최아영/spring/src/main/java/umc/spring/web/dto/MemberRequestDTO.java
@@ -1,6 +1,10 @@
 package umc.spring.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import umc.spring.validation.annotation.ExistCategories;
 
 import java.util.List;
 
@@ -8,12 +12,19 @@ public class MemberRequestDTO {
 
     @Getter
     public static class JoinDto {
+        @NotBlank
         String name;
+        @NotNull
         Integer gender;
+        @NotNull
         Integer birthYear;
+        @NotNull
         Integer birthMonth;
+        @NotNull
         Integer birthDay;
+        @Size(min = 5, max = 12)
         String address;
+        @ExistCategories
         List<Long> favCategory;
     }
 
