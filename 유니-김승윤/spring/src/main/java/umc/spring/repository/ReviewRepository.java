60d95f8359174fdaf744e5d108240e5230a599ed
yunsful <bailey05280@naver.com>
2024-11-21 09:23:22 +0900

    feat2: 레포지토리 추가

--- 유니-김승윤/spring/src/main/java/umc/spring/repository/ReviewRepository.java
+++ 유니-김승윤/spring/src/main/java/umc/spring/repository/ReviewRepository.java
@@ -1,2 +1,7 @@
-package umc.spring.repository;public interface ReviewRepository {
+package umc.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.spring.domain.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
 }
