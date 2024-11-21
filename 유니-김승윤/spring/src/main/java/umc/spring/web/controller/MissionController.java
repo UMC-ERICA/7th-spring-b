1ee2cbb52952d558e978a6a2896275f4ba695082
yunsful <bailey05280@naver.com>
2024-11-21 13:09:39 +0900

    feat4: 컨트롤러 작성

--- 유니-김승윤/spring/src/main/java/umc/spring/web/controller/MissionController.java
+++ 유니-김승윤/spring/src/main/java/umc/spring/web/controller/MissionController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.domain.Mission;
+import umc.spring.domain.mapping.MemberMission;
+import umc.spring.service.missionService.MemberMissionService;
 import umc.spring.service.missionService.MissionService;
+import umc.spring.web.dto.MemberMissionDto;
 import umc.spring.web.dto.MissionDTO;
 
 @RestController
@@ -17,11 +20,19 @@ import umc.spring.web.dto.MissionDTO;
 public class MissionController {
     
     private final MissionService missionService;
+    private final MemberMissionService memberMissionService;
     
     @PostMapping("/")
     public ApiResponse<String> createMission(@RequestBody @Valid MissionDTO.CreateMissionRequestDTO req) {
         missionService.createMission(req);
         
         return ApiResponse.onSuccess("미션 생성됨");
+    }
+    
+    @PostMapping("/challenge")
+    public ApiResponse<String> challengeMission(@RequestBody @Valid MemberMissionDto.AddMissionToMemberRequestDTO req) {
+        memberMissionService.addMissionToMember(req);
+        
+        return ApiResponse.onSuccess("미션 도전하기 추가 완료");
     }
 }
