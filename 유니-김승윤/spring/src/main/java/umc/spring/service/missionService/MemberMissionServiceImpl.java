7d4377df4c7b6d82563a2a280f7afd99d3c12d88
yunsful <bailey05280@naver.com>
2024-11-21 13:08:14 +0900

    feat4: 서비스 로직 추가

--- 유니-김승윤/spring/src/main/java/umc/spring/service/missionService/MemberMissionServiceImpl.java
+++ 유니-김승윤/spring/src/main/java/umc/spring/service/missionService/MemberMissionServiceImpl.java
@@ -2,18 +2,43 @@ package umc.spring.service.missionService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.apiPayload.exception.handler.MemberHandler;
+import umc.spring.apiPayload.exception.handler.MissionHandler;
+import umc.spring.converter.MemberMissionConverter;
+import umc.spring.domain.Member;
+import umc.spring.domain.Mission;
+import umc.spring.domain.mapping.MemberMission;
 import umc.spring.repository.MemberMissionRepository.MemberMissionRepository;
+import umc.spring.repository.MemberRepository.MemberRepository;
+import umc.spring.repository.MissionRepository;
+import umc.spring.web.dto.MemberMissionDto;
 
 @Service
 @RequiredArgsConstructor
-public class MemberMissionService {
+public class MemberMissionServiceImpl implements MemberMissionService {
 
     private final MemberMissionRepository memberMissionRepository;
+    private final MemberRepository memberRepository;
+    private final MissionRepository missionRepository;
+    
 
-    /**
-     * 미션이 회원 미션에 이미 존재하는지 확인
-     */
+    @Override
     public boolean isMissionAssignedToMember(Long missionId) {
         return memberMissionRepository.existsByMissionId(missionId);
+    }
+    
+    @Override
+    public void addMissionToMember(MemberMissionDto.AddMissionToMemberRequestDTO req) {
+        Mission findMission = missionRepository.findById(req.getMissionId()).orElseThrow(
+                () -> new MissionHandler(ErrorStatus.MISSION_NOT_FOUND)
+        );
+        
+        Member findMember = memberRepository.findById(req.getMemberId()).orElseThrow(
+                () -> new MemberHandler(ErrorStatus.MISSION_NOT_FOUND)
+        );
+        MemberMission newMemberMission = MemberMissionConverter.toMemberMission(findMember,findMission);
+        
+        memberMissionRepository.save(newMemberMission);
     }
 }
