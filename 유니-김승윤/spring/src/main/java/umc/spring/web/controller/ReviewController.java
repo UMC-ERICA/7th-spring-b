1fc555c0010fb464270e768d14402499cc5aaf49
yunsful <bailey05280@naver.com>
2024-11-21 09:26:33 +0900

    feat2: 컨트롤러 추가

--- 유니-김승윤/spring/src/main/java/umc/spring/web/controller/ReviewController.java
+++ 유니-김승윤/spring/src/main/java/umc/spring/web/controller/ReviewController.java
@@ -1,2 +1,24 @@
-package umc.spring.web.controller;public class ReviewController {
+package umc.spring.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.service.reviewService.ReviewService;
+import umc.spring.web.dto.ReviewDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reviews")
+public class ReviewController {
+    
+    private final ReviewService reviewService;
+    
+    @PostMapping("/")
+    public ApiResponse<String> addReview(@RequestBody ReviewDTO.ReviewCreateRequestDTO req) {
+        reviewService.createReview(req);
+        return ApiResponse.onSuccess("리뷰가 생성되었습니다.");
+    }
 }
