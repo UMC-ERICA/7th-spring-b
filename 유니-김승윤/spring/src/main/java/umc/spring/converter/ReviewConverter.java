788c63394bf1e4585089fc366af0f52dcc2ee594
yunsful <bailey05280@naver.com>
2024-11-21 09:24:39 +0900

    feat2: converter 구현

--- 유니-김승윤/spring/src/main/java/umc/spring/converter/ReviewConverter.java
+++ 유니-김승윤/spring/src/main/java/umc/spring/converter/ReviewConverter.java
@@ -1,2 +1,15 @@
-package umc.spring.converter;public class ReviewConverter {
+package umc.spring.converter;
+
+import umc.spring.domain.Member;
+import umc.spring.domain.Review;
+import umc.spring.web.dto.ReviewDTO;
+
+public class ReviewConverter {
+    
+    public static Review toReview(ReviewDTO.ReviewCreateRequestDTO req) {
+        return Review.builder()
+                .content(req.getContent())
+                .rating(req.getRating())
+                .build();
+    }
 }
