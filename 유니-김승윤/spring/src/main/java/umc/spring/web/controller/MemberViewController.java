85cbc90ba6c569cc6b3407cae17afe54b7a1bcbc
yunsful <bailey05280@naver.com>
2024-12-30 16:10:05 +0900

    chore: MemberViewController 수정

--- 유니-김승윤/spring/src/main/java/umc/spring/web/controller/MemberViewController.java
+++ 유니-김승윤/spring/src/main/java/umc/spring/web/controller/MemberViewController.java
@@ -1,22 +1,42 @@
 package umc.spring.web.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import umc.spring.service.memberService.MemberCommandService;
 import umc.spring.web.dto.MemberDTO;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberViewController {
-
+    
+    private final MemberCommandService memberCommandService;
+    
+    
     @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
 
-    @GetMapping("/signup")
-    public String signupPage(Model model) {
-        model.addAttribute("memberJoinDto", new MemberDTO.memberJoinDTO());
-        return "signup";
+    @PostMapping("/members/signup")
+    public String joinMember(@ModelAttribute("memberJoinDto") MemberDTO.memberJoinDTO request,
+                             BindingResult bindingResult,
+                             Model model) {
+        if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+        
+        try {
+            memberCommandService.joinMember(request);
+            return "redirect:/login";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "signup";
+        }
     }
 
     @GetMapping("/home")
